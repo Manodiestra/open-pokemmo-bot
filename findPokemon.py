1261b76426638f31f889798d037c78b7e9b9cccc
anthonyreis <matheus.anthony1@gmail.com>
2021-02-24 10:55:32 -0300

    Verificar se ainda existem pokemon vivos

---
+++ findPokemon.py
@@ -0,0 +1,58 @@
+import cv2 as cv
+import numpy as np
+
+haystack_img = cv.imread('poke_img/pokemmo_img.png', cv.IMREAD_UNCHANGED)
+needle_img = cv.imread('poke_img/pokemmo_img2.png', cv.IMREAD_UNCHANGED)
+
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+
+# Get the best match position
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+print('Best match top left position: %s' % str(max_loc))
+print('Best match confidence: %s' % max_val)
+
+threshold = 0.4
+locations = np.where(result >= threshold)
+locations = list(zip(*locations[::-1]))
+
+if locations:
+    print('Found needle.')
+
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
+
+    # Loop over all the locations and draw their rectangle
+    for loc in locations:
+        # Determine the box positions
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        # Draw the box
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
+
+    cv.imshow('Matches', haystack_img)
+    cv.waitKey()
+    #cv.imwrite('result.jpg', haystack_img)
+
+else:
+    print('Needle not found.')
+
+'''
+if max_val >= threshold:
+    print('Found')
+
+    poke_w = needle_img.shape[1]
+    poke_h = needle_img.shape[0]
+
+    top_left = max_loc
+    bottom_right = (top_left[0] + poke_w, top_left[1] + poke_h)
+
+    cv.rectangle(haystack_img, top_left, bottom_right,
+                color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
+    cv.imshow('Result', haystack_img)
+    cv.waitKey()
+else:
+    print('Not found')
+'''
