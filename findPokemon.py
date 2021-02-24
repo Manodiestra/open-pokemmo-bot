e0c25853d8f6770a4e7ab3ec856dad3ef831343b
anthonyreis <matheus.anthony1@gmail.com>
2021-02-24 11:19:09 -0300

    Agrupamento de retangulos

--- findPokemon.py
+++ findPokemon.py
@@ -1,8 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-haystack_img = cv.imread('poke_img/pokemmo_img.png', cv.IMREAD_UNCHANGED)
-needle_img = cv.imread('poke_img/pokemmo_img2.png', cv.IMREAD_UNCHANGED)
+haystack_img = cv.imread('poke_img/pokemmo_horda_shelmet.png', cv.IMREAD_UNCHANGED)
+needle_img = cv.imread('poke_img/pokemmo_single_shelmet.png', cv.IMREAD_UNCHANGED)
+
+needle_w = needle_img.shape[1]
+needle_h = needle_img.shape[0]
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
 
@@ -12,23 +15,28 @@ min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
 print('Best match top left position: %s' % str(max_loc))
 print('Best match confidence: %s' % max_val)
 
-threshold = 0.4
+threshold = 0.5
 locations = np.where(result >= threshold)
 locations = list(zip(*locations[::-1]))
 
-if locations:
+rectangles = []
+for loc in locations:
+    rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+    rectangles.append(rect)
+
+rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+
+if len(rectangles):
     print('Found needle.')
 
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
     line_color = (0, 255, 0)
     line_type = cv.LINE_4
 
     # Loop over all the locations and draw their rectangle
-    for loc in locations:
+    for (x, y, w, h) in rectangles:
         # Determine the box positions
-        top_left = loc
-        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        top_left = (x, y)
+        bottom_right = (x + w, y + h)
         # Draw the box
         cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
 
@@ -37,4 +45,3 @@ if locations:
 
 else:
     print('Needle not found.')
-    
